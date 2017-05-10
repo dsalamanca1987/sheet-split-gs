31071eb4c97477559411bbf62a7fdc3246a0f29b
Andrew Hsu <andrewhsu42@gmail.com>
2017-05-10 16:04:04 -0700

    Implement functions to extract data by product name

--- splitFunctions.js
+++ splitFunctions.js
@@ -18,7 +18,45 @@ function sanitizeData(colId, tgtString) {
   dataRange.setValues(newData);
 }
 
-function splitData() {
+function getData(sheet) {
+  sheet = sheet || SpreadsheetApp.getActiveSheet();
+  var startRow = 2;  // First row of data to process
+  var startCol = 1;
+  var numRows = sheet.getLastRow();   // Number of rows to process
+  var numCols = sheet.getLastColumn();
+  var dataRange = sheet.getRange(startRow, startCol, numRows, numCols);
+  return dataRange.getValues();
+}
+
+function runSplit() {
   var sheet = SpreadsheetApp.getActiveSheet();
-  SpreadsheetApp.getUi().alert('Yes?');
+  //var ui = SpreadsheetApp.getUi();
+  //var response = ui.alert('Are you sure you want to proceed?');
+  // Process the user's response.
+  //if (response == ui.Button.YES) {
+  var dataObject = getSplitData(getData());
+  //Logger.log(dataObject);
+  var singleData = getDataByName(dataObject, "Pic Collage - Photo Collage Maker & Picture Editor");
+  Logger.log(JSON.stringify(singleData));
+  
+  //}
+  return;
+}
+
+function getSplitData(data, colId) {
+  // Returns object of data with product name arrays
+  colId = colId || 3;
+  var dataObj = {};
+  data.forEach(function(item, i) {
+    if (dataObj.hasOwnProperty(item[colId])) {
+      dataObj[item[colId]].push(data[i])
+    } else {
+      dataObj[item[colId]] = [];
+    }
+  });
+  return dataObj;
+}
+
+function getDataByName(dataObj, name) {
+  return dataObj[name];
 }
