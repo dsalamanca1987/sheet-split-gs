cd014785f0e60d0e6da5657c520ef3d2380e61fe
Andrew Hsu <andrewhsu42@gmail.com>
2017-05-10 14:20:47 -0700

    Implement backup sheet protection and hiding

--- fileFunctions.js
+++ fileFunctions.js
@@ -8,5 +8,23 @@ function backupSheet(sheetName) {
     Logger.log('Backup already exists');
     return;
   }
-  sheet.copyTo(ss).setName(backupName);
+  sheet.copyTo(ss).setName(backupName).hideSheet();
+  protectSheet(backupName);
+  Logger.log('Backup created');
+}
+
+function protectSheet(sheetName) {
+  // Protect the active sheet, then remove all other users from the list of editors.
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = ss.getSheetByName(sheetName) || ss.getActiveSheet();
+  var protection = sheet.protect().setDescription('Sample protected sheet');
+
+  // Ensure the current user is an editor before removing others. Otherwise, if the user's edit
+  // permission comes from a group, the script will throw an exception upon removing the group.
+  var me = Session.getEffectiveUser();
+  protection.addEditor(me);
+  protection.removeEditors(protection.getEditors());
+  if (protection.canDomainEdit()) {
+   protection.setDomainEdit(false);
+  }
 }
